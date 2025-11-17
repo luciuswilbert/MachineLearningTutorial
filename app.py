44cdf49d0f86dde94a9a579cc6b14a8d2d9ecb81
luciuswilbert <luciuswilbert@gmail.com>
2025-11-17 19:49:38 +0700

    Matplotlib

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
+# help(plt.hist)
 
-year = [1990, 1995, 2000, 2005, 2010, 2015, 2020, 2025]
-population = [5.6, 5.9, 6.3, 6.7, 7.2, 7.5, 7.6, 7.4]
+values = [0, 0.6, 1.4, 1.6, 2.2, 2.5, 2.6, 3.2, 3.5, 3.9, 4.2, 6]
 
-plt.plot(year, population)
+plt.hist(values, bins=12)
 plt.show()
