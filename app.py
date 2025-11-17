d679a0c181ab323fbd2b18cebfa3adaedfd4ad5a
luciuswilbert <luciuswilbert@gmail.com>
2025-11-17 20:10:41 +0700

    Matplotlib

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-plt.plot([1, 2, 3])     # first plot
-plt.show()
-plt.clf()               # clear the figure
+year = list(range(1950, 2101))
+population = np.linspace(2.538, 10.85, len(year))
 
-plt.plot([3, 2, 1])     # second plot
+# Adding more data
+year = [1800, 1850, 1900] + year
+population = [1.0, 1.262, 1.650] + list(population)
+
+plt.plot(year, population)
+plt.xlabel('Year')
+plt.ylabel('Population')
+plt.title('World Population Projection')
+plt.yticks([0, 2, 4, 6, 8, 10],
+           ['0', '2B', '4B', '6B', '8B', '10B'])
+
 plt.show()
-plt.clf()               # clear again (optional)
