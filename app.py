5392dcf91057e36ee96421f354f727ba1fbdb289
luciuswilbert <luciuswilbert@gmail.com>
2025-11-21 21:27:21 +0800

    Kaggle

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from sklearn.tree import DecisionTreeRegressor
+
 import pandas as pd
 
 melbourne_data = pd.read_csv("melb_data.csv")
@@ -12,3 +14,12 @@ melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude'
 x = melbourne_data[melbourne_features]
 print(x.describe())
 print(x.head())
+
+melbourne_model = DecisionTreeRegressor(random_state=1)
+
+melbourne_model.fit(x, y)
+
+print("Making predictions for the following 5 houses:")
+print(x.head())
+print("The predictions are")
+print(melbourne_model.predict(x.head()))
