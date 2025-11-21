fee76a30f5540fdb9929e83ff727f110c5dd5402
luciuswilbert <luciuswilbert@gmail.com>
2025-11-21 21:12:47 +0800

    Kaggle

--- app.py
+++ app.py
@@ -3,3 +3,12 @@ import pandas as pd
 melbourne_data = pd.read_csv("melb_data.csv")
 print(melbourne_data.describe())
 print(melbourne_data.columns)
+
+melbourne_data = melbourne_data.dropna(axis=0)
+print(melbourne_data.describe())
+
+y = melbourne_data.Price
+melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
+x = melbourne_data[melbourne_features]
+print(x.describe())
+print(x.head())
