c556099e14ea781fda40439847bf3a3b4883cb66
luciuswilbert <luciuswilbert@gmail.com>
2025-11-23 19:17:33 +0800

    Decision Tree

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ import pandas as pd
 def get_mae(max_leaf_nodes, train_x, val_x, train_y, val_y):
     model = DecisionTreeRegressor(max_leaf_nodes = max_leaf_nodes, random_state = 1)
     model.fit(train_x, train_y)
-    prediction = model.predict(val_y)
+    prediction = model.predict(val_x)
+    mae = mean_absolute_error(val_y, prediction)
+    return mae
 
 
 melbourne_data = pd.read_csv("melb_data.csv")
@@ -21,14 +23,6 @@ x = melbourne_data[melbourne_features]
 # Splitting
 train_x, val_x, train_y, val_y = train_test_split(x, y, random_state = 42)
 
-melbourne_model = DecisionTreeRegressor(random_state=1)
-
-melbourne_model.fit(train_x, train_y)
-
-# print("Making predictions for the following 5 houses:")
-# print(x.head())
-# print("The predictions are")
-# print(melbourne_model.predict(x.head()))
-
-predicted_home_prices = melbourne_model.predict(val_x)
-print("Mean Absolute Error: ", mean_absolute_error(val_y, predicted_home_prices))
+for max_leaf_nodes in [5, 50, 500, 5000]:
+    my_mae = get_mae(max_leaf_nodes, train_x, val_x, train_y, val_y)
+    print(f"Max leaf nodes: {max_leaf_nodes} \nMean Absolute Error: {my_mae}")
