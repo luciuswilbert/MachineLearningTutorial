836628470e3f2e9c67d67688d974c1711913e2db
luciuswilbert <luciuswilbert@gmail.com>
2025-11-23 19:21:30 +0800

    Random Forest

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
 
 import pandas as pd
 
@@ -10,7 +11,6 @@ def get_mae(max_leaf_nodes, train_x, val_x, train_y, val_y):
     prediction = model.predict(val_x)
     mae = mean_absolute_error(val_y, prediction)
     return mae
-
 
 melbourne_data = pd.read_csv("melb_data.csv")
 
@@ -23,6 +23,11 @@ x = melbourne_data[melbourne_features]
 # Splitting
 train_x, val_x, train_y, val_y = train_test_split(x, y, random_state = 42)
 
-for max_leaf_nodes in [5, 50, 500, 5000]:
-    my_mae = get_mae(max_leaf_nodes, train_x, val_x, train_y, val_y)
-    print(f"Max leaf nodes: {max_leaf_nodes} \nMean Absolute Error: {my_mae}")
+# for max_leaf_nodes in [5, 50, 500, 5000]:
+#     my_mae = get_mae(max_leaf_nodes, train_x, val_x, train_y, val_y)
+#     print(f"Max leaf nodes: {max_leaf_nodes} \nMean Absolute Error: {my_mae}")
+
+forest_model = RandomForestRegressor(random_state = 1)
+forest_model.fit(train_x, train_y)
+predictions = forest_model.predict(val_x)
+print(mean_absolute_error(val_y, predictions))
