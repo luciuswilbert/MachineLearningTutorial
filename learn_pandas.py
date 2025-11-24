4096e28c10003a49a7aec6b6fc9eb5f1f3c70951
luciuswilbert <luciuswilbert@gmail.com>
2025-11-24 09:00:05 +0800

    Pandas Series

--- learn_pandas.py
+++ learn_pandas.py
@@ -1,5 +1,15 @@
 import pandas as pd
 
 data_frame = pd.DataFrame({'Yes': [50, 21], 'No': [131, 2]})
+data_frame2 = pd.DataFrame({'Bob': ['I liked it.', 'It was awful'], 'Sue': ['Pretty good.', 'Bland']})
+data_frame3 = pd.DataFrame({'Bob': ['I liked it.', 'It was awful'], 'Sue': ['Pretty good.', 'Bland']}, index=['Product A', 'Product B'])
+
+series_1 = pd.Series([1, 2, 3, 4, 5])
+series_2 = pd.Series([1, 2, 3], index=['2015 Sales', '2020 Sales', '2025 Sales'], name='Product A')
 
 print(data_frame)
+print(data_frame2)
+print(data_frame3)
+
+print(series_1)
+print(series_2)
