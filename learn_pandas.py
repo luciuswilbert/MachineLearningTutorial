30f04b71d89271185078f67df55c2dba2a87d4dd
luciuswilbert <luciuswilbert@gmail.com>
2025-11-25 13:04:14 +0800

    Pandas Data Types and Missing Values

--- learn_pandas.py
+++ learn_pandas.py
@@ -3,5 +3,6 @@ import pandas as pd
 sample_data = pd.read_csv('sample.csv')
 # print(sample_data.Region.value_counts())
 # print(sample_data.groupby('Region').Region.count())
-
-
+# print(sample_data.sort_values(by='len', ascending = False))
+print(sample_data.dtypes)
+print(sample_data.UnitsSold.dtype)
