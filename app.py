f76ce70ad7ab5b4983845330a3922458290b36a3
luciuswilbert <luciuswilbert@gmail.com>
2025-11-26 13:43:30 +0800

    Data Pipeline

--- app.py
+++ app.py
@@ -2,5 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 sample_data = pd.read_csv('sample.csv')
-sample_data.dropna(axis=0, subset=['CostPerUnit'], inplace=True)
+# sample_data.dropna(axis=0, subset=['CostPerUnit'], inplace=True)
+# print(sample_data)
+sample_data = sample_data.select_dtypes(exclude=['object'])
 print(sample_data)
