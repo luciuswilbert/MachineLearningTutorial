25e7cee9b16ccf20bbef1c65d02d0c56c952ecb6
luciuswilbert <luciuswilbert@gmail.com>
2025-11-27 16:02:37 +0800

    Data Pipeline

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ import pandas as pd
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
+
+my_pipeline = Pipeline(steps=[('imputer', SimpleImputer(strategy='median')),
+                              
+                              ])
