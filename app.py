d8cefe4cec87612dc57701b5b33438d186eb0e41
luciuswilbert <luciuswilbert@gmail.com>
2025-11-27 16:19:09 +0800

    Data Pipeline

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
 
 my_pipeline = Pipeline(steps=[('imputer', SimpleImputer(strategy='median')),
-                              ('scaler', StandardScaler())
+                              ('scaler', StandardScaler()),
+                              ('model', RandomForestRegressor(n_estimators=50, random_state=42))
                               ])
