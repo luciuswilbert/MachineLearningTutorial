f01474473702c23066b1888239e1d1cac2e3d38b
luciuswilbert <luciuswilbert@gmail.com>
2025-11-27 16:35:59 +0800

    Data Pipeline

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 
-my_pipeline = Pipeline(steps=[('imputer', SimpleImputer(strategy='median')),
+numerical_transformer = Pipeline(steps=[('imputer', SimpleImputer(strategy='median')),
                               ('scaler', StandardScaler()),
-                              ('model', RandomForestRegressor(n_estimators=50, random_state=42))
+                              ])
+categorical_transformer = Pipeline(steps=[('imputer', SimpleImputer(strategy='most_frequent')),
+                              ('one_hot', OneHotEncoder()),
                               ])
