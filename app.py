067cdceeff4516b8a15e71d2facd3d3ecd8f89b4
luciuswilbert <luciuswilbert@gmail.com>
2025-11-30 19:01:36 +0800

    Data Visualization

--- app.py
+++ app.py
@@ -3,4 +3,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 fifa_data = pd.read_csv('fifa.csv', index_col='Date', parse_dates=True)
-print(fifa_data.head())
+
+plt.figure(figsize=(12,6))
+sns.lineplot(data=fifa_data)
+plt.show()
